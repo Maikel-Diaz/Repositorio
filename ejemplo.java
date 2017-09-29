9f41851dab81b0905f0264cc1cc7112f9ea4d2e6
Michael <maikel_20@hotmail.es>
2017-09-29 12:50:13 +0200

    Localidad añadida

--- ejemplo.java
+++ ejemplo.java
@@ -2,6 +2,7 @@ public class ejemplo{
  public static void main(String args[]){
  
 	System.out.println("Michael Díaz González");
-	System.out.println("19 años")
+	System.out.println("19 años");
+	System.out.println("León");
  }
 }
